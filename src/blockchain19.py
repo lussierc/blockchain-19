c4280d5a874a0ae57854f7107804c1dc012a817f
Christian Lussier <lussierc@allegheny.edu>
2021-01-29 12:28:03 -0500

    Implement storage functionality for new ledgers
    Co-Authored-By: RyanHiltyAllegheny <hiltyr@allegheny.edu>
    Co-Authored-By: wilsonj24 <wilsonj3@allegheny.edu>

--- src/blockchain19.py
+++ src/blockchain19.py
@@ -20,19 +20,29 @@ def create_ledger():
     print("** CREATING LEDGER....")
 
     done_adding = False
-    current_patient_dict = {}
+    patient_blocks = []
+    current_patient_dict = {'hospital': '', 'patient': '', 'status': '', 'nonce': '', 'a': '', 'b': '', 'c': '', 'hash': ''}
+
     while done_adding is False:
         hospital_input = input(" - Enter Patient Hospital: ")
         patient_id_input = input(" - Enter Patient ID: ")
         patient_status = input(" - Enter the Patient's Status: ")
 
+        current_patient_dict['hospital'] = hospital_input
+        current_patient_dict['patient'] = patient_id_input
+        current_patient_dict['status'] = patient_status
+
+        print(current_patient_dict)
+        patient_blocks.append(current_patient_dict)
+
         new_block = input("*** Would you like to add another block to the blockchain? Y or N: ")
         if str(new_block) == 'Y':
             pass
-            current_patient_dict = {}
+            current_patient_dict = {'hospital': '', 'patient': '', 'status': '', 'nonce': '', 'a': '', 'b': '', 'c': '', 'hash': ''}
         else:
             done_adding = True
-
+    print("PRINTING LIST OF PATIENT BLOCKS: ", patient_blocks)
+    
 def import_ledger():
     """Imports a previously exported ledger."""
     print("** IMPORTING LEDGER....")
