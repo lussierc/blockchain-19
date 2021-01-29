07e844bbc5c3c1bb563d4d0c80f0cf4b4a3a5fb1
Christian Lussier <lussierc@allegheny.edu>
2021-01-29 12:23:03 -0500

    Allow users to enter info needed for a new block in a new ledger
    Co-Authored-By: RyanHiltyAllegheny <hiltyr@allegheny.edu>
    Co-Authored-By: wilsonj24 <wilsonj3@allegheny.edu>

--- src/blockchain19.py
+++ src/blockchain19.py
@@ -2,7 +2,7 @@
 
 def main():
     """The main driver function for the project."""
-    
+
     user_choice = input(" - Enter 1 to Create a New Ledger.\n - Enter 2 to Import a Previously Exported Ledger.\nEnter your choice: ")
 
     if int(user_choice) == 1:
@@ -18,6 +18,20 @@ def main():
 def create_ledger():
     """Creates a new ledger."""
     print("** CREATING LEDGER....")
+
+    done_adding = False
+    current_patient_dict = {}
+    while done_adding is False:
+        hospital_input = input(" - Enter Patient Hospital: ")
+        patient_id_input = input(" - Enter Patient ID: ")
+        patient_status = input(" - Enter the Patient's Status: ")
+
+        new_block = input("*** Would you like to add another block to the blockchain? Y or N: ")
+        if str(new_block) == 'Y':
+            pass
+            current_patient_dict = {}
+        else:
+            done_adding = True
 
 def import_ledger():
     """Imports a previously exported ledger."""
