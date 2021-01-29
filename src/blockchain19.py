895249b7e87ebfec3d8d4e49e27475c03a9f699a
Christian Lussier <lussierc@allegheny.edu>
2021-01-29 12:09:31 -0500

    Add base implementation for program

--- src/blockchain19.py
+++ src/blockchain19.py
@@ -1,1 +1,24 @@
 # Base file
+
+def main():
+    user_choice = input(" - Enter 1 to Create a New Ledger.\n - Enter 2 to Import a Previously Exported Ledger.\nEnter your choice: ")
+
+    if int(user_choice) == 1:
+        print("Creating a new ledger!")
+        create_ledger()
+    elif int(user_choice) == 2:
+        print("Importing a previous ledger!")
+        import_ledger()
+    else:
+        print("Invalid option. Creating a new ledger!")
+        create_ledger()
+
+def create_ledger():
+    """Creates a new ledger."""
+    print("** CREATING LEDGER....")
+
+def import_ledger():
+    """Imports a previously exported ledger."""
+    print("** IMPORTING LEDGER....")
+
+main()
