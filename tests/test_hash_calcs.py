8d58c77772dc98e1ac1e788aac00e8d832d98a66
Christian Lussier <lussierc@allegheny.edu>
2021-02-11 13:36:42 -0500

    Write preliminary test case for find_nonce()

--- tests/test_hash_calcs.py
+++ tests/test_hash_calcs.py
@@ -9,3 +9,11 @@ def test_find_first_letter():
     first_letter = hash_calcs.find_first_letter(letter)
 
     assert letter == first_letter
+
+def find_nonce():
+    intermediate_hash = 204
+    correct_nonce = # the manually calculated correct nonce
+
+    nonce = hash_calcs.find_nonce(intermediate_hash)
+
+    assert nonce == correct_nonce
