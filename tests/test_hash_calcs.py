ed4be7354e08a72531f145e4ed8ff1642dd9a6ad
Christian Lussier <lussierc@allegheny.edu>
2021-02-11 13:43:21 -0500

    Fix test_find_nonce() function

--- tests/test_hash_calcs.py
+++ tests/test_hash_calcs.py
@@ -11,8 +11,8 @@ def test_find_first_letter():
     assert letter == first_letter
 
 def find_nonce():
-    intermediate_hash = 204
-    correct_nonce = # the manually calculated correct nonce
+    intermediate_hash = 206
+    correct_nonce = 1 # the manually calculated correct nonce
 
     nonce = hash_calcs.find_nonce(intermediate_hash)
 
