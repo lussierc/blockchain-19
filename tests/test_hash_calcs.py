1efbf1518b58c00084242e6fa4a78b03422d3e2c
Christian Lussier <lussierc@allegheny.edu>
2021-02-11 13:56:34 -0500

    Add paramaterized test case to test the correct calculation of multiple nonces

--- tests/test_hash_calcs.py
+++ tests/test_hash_calcs.py
@@ -3,12 +3,21 @@
 import pytest
 from src import hash_calcs
 
-def test_find_first_letter():
+def test_find_first_letter_lower():
 
     letter = "a"
     first_letter = hash_calcs.find_first_letter(letter)
 
     assert letter == first_letter
+
+
+def test_find_first_letter_upper():
+
+    letter = "A"
+    first_letter = hash_calcs.find_first_letter(letter)
+
+    assert letter == first_letter
+
 
 def test_find_nonce():
     intermediate_hash = 206
@@ -17,3 +26,18 @@ def test_find_nonce():
     nonce = hash_calcs.find_nonce(intermediate_hash)
 
     assert nonce == correct_nonce
+
+
+@pytest.mark.parametrize(
+    "example_int_hashes, expected_nonces",
+    [([206, 203, 198, 163], [1, 1, 3, 2])],
+)
+def test_find_nonce_multiples(example_int_hashes, expected_nonces):
+
+    calculated_nonces = []
+
+    for example_int_hash in example_int_hashes:
+        calculated_nonce = hash_calcs.find_nonce(example_int_hash)
+        calculated_nonces.append(calculated_nonce)
+
+    assert calculated_nonces == expected_nonces
